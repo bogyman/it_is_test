7705ae07aac5991a76777391bded0d8ce022c495
Korzhik Evgen <kevgen@kickseed.dev.jbs.com.ua>
2013-04-10 09:39:00 +0300

    fixed queries

--- it/3/queries.sql
+++ it/3/queries.sql
@@ -1,3 +1,4 @@
+
 SELECT count(*) 
 FROM (
     SELECT DISTINCT boss_id 
@@ -8,16 +9,10 @@ FROM (
 
 
 
-SELECT name 
-FROM person 
-WHERE id IN (
-    SELECT DISTINCT boss_id 
-    FROM person 
-    GROUP BY boss_id 
-    HAVING boss_id <> 0
-    ) 
-ORDER BY name;
-
+SELECT DISTINCT p1.name
+FROM person p1
+INNER JOIN  person p2 ON p1.id = p2.boss_id 
+ORDER BY p1.name;
 
 
 SELECT DISTINCT boss_id, count(boss_id) 
@@ -25,6 +20,16 @@ FROM person
 GROUP BY boss_id 
 HAVING  boss_id <> 0  ;
 
+
+
+SELECT p1.boss_id, p1.name, p1.id 
+FROM person p1 INNER JOIN person p2 ON p1.id=p2.boss_id
+GROUP BY p1.boss_id, p1.name, p1.id 
+HAVING p1.boss_id <> 0 
+    AND count(p1.boss_id) > 2 
+ORDER BY p1.name;
+
+-- or
 
 SELECT * FROM person 
 WHERE id IN (
